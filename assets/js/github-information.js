6191a41e0b7274543fa4a1147e741625ae840705
Chris Zielinski <ckz8780@gmail.com>
2018-05-15 19:49:51 +0000

    added rate limiter on github api requests

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -36,6 +36,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $('#gh-user-data').html('');
+    $('#gh-user-data').html('');
     var username = $("#gh-username").val();
     if(!username) {
         $('#gh-user-data').html(`<h2>Please enter a Github username</h2>`);
@@ -59,9 +61,14 @@ function fetchGitHubInformation(event) {
     }, function(errorResponse) {
         if(errorResponse.status == 404) {
             $('#gh-user-data').html(`<h2>No info found for user ${username}.</h2>`)
+        } else if (errorResponse.status == 403) {
+            var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+            $('#gh-user-data').html(`<h4>Too many requests! Please wait until ${resetTime.toLocaleTimeString()}</h4>`)
         } else {
             console.log(errorResponse);
             $('#gh-user-data').html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
         }
     });
 }
+
+$(document).ready(fetchGitHubInformation);
